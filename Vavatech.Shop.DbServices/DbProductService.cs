27789ffb84890f08ba3061350b6ca1f431374ef3
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-09-20 17:15:51 +0200

    Pobieranie produktów z bazy danych

---
+++ Vavatech.Shop.DbServices/DbProductService.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using Vavatech.Shop.IServices;
+using Vavatech.Shop.Models;
+using Vavatech.Shop.Models.SearchCriterias;
+
+namespace Vavatech.Shop.DbServices
+{
+    public class DbProductService : IProductService
+    {
+        private readonly DbConnection connection;
+
+        public DbProductService(DbConnection connection)
+        {
+            this.connection = connection;
+        }
+
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> Get(string color)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> Get(decimal? fromUnitPrice, decimal? toUnitPrice)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> Get(ProductSearchCriteria searchCriteria)
+        {
+            string sql = "SELECT TOP 1000 .... WHERE Name LIKE @Name";
+
+            SqlCommand command = new SqlCommand();
+            command.CommandText = sql;
+            command.Parameters.AddWithValue("@Name", searchCriteria.NameFrom);
+            command.Parameters.AddWithValue("@Color", searchCriteria.Color);
+
+            ICollection<Product> products = new List<Product>();
+
+            var reader = command.ExecuteReader();
+
+            while(reader.Read())
+            {
+                Product product = new Product
+                {
+                    Name = reader.GetString(0),
+                    Color = reader.GetString(1),
+                };
+
+                products.Add(product);
+            }
+
+            return products;
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
