1dc9ab28428fad0fb9d9165ce532911ab5b678d0
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-09-20 17:16:10 +0200

    Wstrzykiwanie zależności

--- Vavatech.Shop.WPFClient/ViewModelLocator.cs
+++ Vavatech.Shop.WPFClient/ViewModelLocator.cs
@@ -1,22 +1,38 @@
-﻿using System;
+﻿using Bogus;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Unity;
 using Vavatech.Shop.FakeServices;
 using Vavatech.Shop.FakeServices.Fakers;
+using Vavatech.Shop.IServices;
+using Vavatech.Shop.Models;
 using Vavatech.Shop.ViewModels;
 
 namespace Vavatech.Shop.WPFClient
 {
+    // Install-Package Unity
     public class ViewModelLocator
     {
+        private readonly IUnityContainer container;
 
         public ViewModelLocator()
         {
+            container = new UnityContainer();
 
+            container.RegisterType<ProductsViewModel>();
+            container.RegisterType<IProductService, FakeProductService>();
+            container.RegisterType<Faker<Product>, ProductFaker>();
         }
 
-        public ProductsViewModel ProductViewModel => new ProductsViewModel(new FakeProductService(new ProductFaker()));
+        // public ProductsViewModel ProductViewModel => new ProductsViewModel(new FakeProductService(new ProductFaker()));
+
+        public ProductsViewModel ProductsViewModel => container.Resolve<ProductsViewModel>();
+
+
     }
+
+   
 }
