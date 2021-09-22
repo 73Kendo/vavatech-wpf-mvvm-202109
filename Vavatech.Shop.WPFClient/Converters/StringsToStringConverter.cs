2273189d10509dda93ab5fecf3bc6840d6cb39fe
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-09-22 17:13:30 +0200

    Konwertery

---
+++ Vavatech.Shop.WPFClient/Converters/StringsToStringConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Vavatech.Shop.WPFClient.Converters
+{
+    public class StringsToStringConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            string result = string.Empty;
+
+            foreach (var value in values)
+            {
+                result += value + " ";
+            }
+
+            return result;
+            
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Vavatech.Shop.WPFClient/Converters/UnitPriceToBrushConverter.cs
+++ Vavatech.Shop.WPFClient/Converters/UnitPriceToBrushConverter.cs
@@ -6,27 +6,6 @@ using System.Windows.Media;
 namespace Vavatech.Shop.WPFClient.Converters
 {
 
-    public class StringsToStringConverter : IMultiValueConverter
-    {
-        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
-        {
-            string result = string.Empty;
-
-            foreach (var value in values)
-            {
-                result += value + " ";
-            }
-
-            return result;
-            
-        }
-
-        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
     public class UnitPriceToBrushConverter : IValueConverter
     {
         private readonly Brush brush1;
