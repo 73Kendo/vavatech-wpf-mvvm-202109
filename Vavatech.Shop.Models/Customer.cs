ad121fa296230612f18fcdaaa25c65cb8a245280
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-09-24 17:48:28 +0200

    Walidacje

--- Vavatech.Shop.Models/Customer.cs
+++ Vavatech.Shop.Models/Customer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,12 +15,14 @@ namespace Vavatech.Shop.Models
         public double Y { get; set; }
     }
 
-    public class Customer : BaseEntity
+    public class Customer : BaseEntity, IDataErrorInfo, INotifyDataErrorInfo
     {
         private string firstName;
         private string lastName;
         private byte progress;
         private bool isRemoved;
+
+        
 
         public string FirstName
         {
@@ -65,6 +69,54 @@ namespace Vavatech.Shop.Models
 
         public Address WorkAddress { get; set; }
         public Address HomeAddress  { get; set; }
+
+        #region IDataErrorInfo
+
+        public string this[string columnName]
+        {
+            get
+            {
+                if (columnName == nameof(FirstName))
+                {
+                    if (string.IsNullOrEmpty(this.FirstName))
+                    {
+                        return "Tekst nie może być jest pusty";
+                    }
+                }
+
+                if (columnName == nameof(LastName))
+                {
+                    if (string.IsNullOrEmpty(this.LastName))
+                    {
+                        return "Tekst nie może być jest pusty";
+                    }
+                }
+
+
+                return string.Empty;
+
+            }
+        }
+
+        public string Error => string.Empty;
+
+        #endregion
+
+        #region INotifyDataErrorInfo
+
+        // https://kmatyaszek.github.io/wpf%20validation/2019/03/13/wpf-validation-using-inotifydataerrorinfo.html
+        public bool HasErrors => false;
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            return null;
+        }
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        #endregion  
+
+
     }
 
     public enum Gender
---
+++ Vavatech.Shop.Models/Validators/CustomerValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vavatech.Shop.Models.Validators
+{
+    // Install-Package Fluentvalidation
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(p => p.FirstName).NotEmpty();
+        }
+    }
+}
